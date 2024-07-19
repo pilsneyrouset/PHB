005d5c4b96d98ce6abb9019a337f5d642af3c53d
pilsneyrouset <nils.peyrouset@ensae.fr>
2024-07-19 10:41:16 +0200

    add zeta in test interpolation_minmax

--- tests/interpolation_minmax.py
+++ tests/interpolation_minmax.py
@@ -15,28 +15,28 @@ class TestInterpolationMinmax(unittest.TestCase):
         self.thresholds = np.array([self.alpha * k / self.m for k in range(self.K)])
         self.kmin = np.random.randint(low=0, high=self.K)
         self.kmax = np.random.randint(low=self.kmin, high=self.K)
+        self.zeta = [k for k in range(self.K)]
     
     def test_constant_pvalues(self):
         p_values = [0] * self.s
-        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax), interpolation_minmax_naif(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax))
+        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax, zeta=self.zeta), interpolation_minmax_naif(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax, zeta=self.zeta))
 
         p_values = [1] * self.s
-        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax), interpolation_minmax_naif(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax))
+        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax, zeta=self.zeta), interpolation_minmax_naif(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax, zeta=self.zeta))
 
         p_values = [0.3] * self.s
-        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax), interpolation_minmax_naif(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax))
-    
+        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax, zeta=self.zeta), interpolation_minmax_naif(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax, zeta=self.zeta))
     def test_random_pvalues(self):
         p_values = np.random.uniform(low=0, high=1, size=self.s)
-        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax), interpolation_minmax_naif(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax))
+        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax, zeta=self.zeta), interpolation_minmax_naif(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax, zeta=self.zeta))
 
         p_values = np.random.beta(a=self.K, b=self.m - self.K + 1, size=self.s)
-        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax), interpolation_minmax_naif(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax))
+        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax, zeta=self.zeta), interpolation_minmax_naif(p_values, self.thresholds, kmin=self.kmin, kmax=self.kmax, zeta=self.zeta))
     
     def test_indices(self):
         p_values = np.random.beta(a=self.K, b=self.m - self.K + 1, size=self.s)
-        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=self.K, kmax=self.K), interpolation_minmax_naif(p_values, self.thresholds, kmin=self.K, kmax=self.K))
-        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=0, kmax=0), interpolation_minmax_naif(p_values, self.thresholds, kmin=0, kmax=0))
+        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=self.K, kmax=self.K, zeta=self.zeta), interpolation_minmax_naif(p_values, self.thresholds, kmin=self.K, kmax=self.K, zeta=self.zeta))
+        self.assertEqual(interpolation_minmax(p_values, self.thresholds, kmin=0, kmax=0, zeta=self.zeta), interpolation_minmax_naif(p_values, self.thresholds, kmin=0, kmax=0, zeta=self.zeta))
 
 
 if __name__ == '__main__':
