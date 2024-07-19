2afd89c7c09cba0dd608adfba3d9de9c128a8ea9
pilsneyrouset <nils.peyrouset@ensae.fr>
2024-07-19 10:23:38 +0200

    add zeta in test linear interpolation

--- tests/linear_interpolation.py
+++ tests/linear_interpolation.py
@@ -15,28 +15,29 @@ class TestLinearInterpolation(unittest.TestCase):
         self.s = 600  # length of the region we are interested in
         self.thresholds = np.array([self.alpha * k / self.m for k in range(self.K)])
         self.kmin = np.random.randint(low=1, high=self.K)
+        self.zeta = [k for k in range(self.K)]
     
     def test_constant_pvalues(self):
         p_values = [0] * self.s
-        self.assertEqual(assert_almost_equal(linear_interpolation(p_values, self.thresholds),[interpolation(np.sort(p_values)[:i], self.thresholds) for i in range(1, self.s+1)] ), None)
-        self.assertEqual(linear_interpolation(p_values, self.thresholds, kmin=self.kmin)[-1], interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.K))
+        self.assertEqual(assert_almost_equal(linear_interpolation(p_values, self.thresholds),[interpolation(np.sort(p_values)[:i], self.thresholds, zeta=self.zeta) for i in range(1, self.s+1)] ), None)
+        self.assertEqual(linear_interpolation(p_values, self.thresholds, kmin=self.kmin)[-1], interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.K, zeta=self.zeta))
 
         p_values = [1] * self.s
-        self.assertEqual(assert_almost_equal(linear_interpolation(p_values, self.thresholds),[interpolation(np.sort(p_values)[:i], self.thresholds) for i in range(1, self.s+1)] ), None)
-        self.assertEqual(linear_interpolation(p_values, self.thresholds, kmin=self.kmin)[-1], interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.K))
+        self.assertEqual(assert_almost_equal(linear_interpolation(p_values, self.thresholds),[interpolation(np.sort(p_values)[:i], self.thresholds, zeta=self.zeta) for i in range(1, self.s+1)] ), None)
+        self.assertEqual(linear_interpolation(p_values, self.thresholds, kmin=self.kmin)[-1], interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.K, zeta=self.zeta))
 
         p_values = [0.3] * self.s
-        self.assertEqual(assert_almost_equal(linear_interpolation(p_values, self.thresholds),[interpolation(np.sort(p_values)[:i], self.thresholds) for i in range(1, self.s+1)] ), None)
-        self.assertEqual(linear_interpolation(p_values, self.thresholds, kmin=self.kmin)[-1], interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.K))
+        self.assertEqual(assert_almost_equal(linear_interpolation(p_values, self.thresholds),[interpolation(np.sort(p_values)[:i], self.thresholds, zeta=self.zeta) for i in range(1, self.s+1)] ), None)
+        self.assertEqual(linear_interpolation(p_values, self.thresholds, kmin=self.kmin)[-1], interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.K, zeta=self.zeta))
     
     def test_random_pvalues(self):
         p_values = np.random.uniform(low=0, high=1, size=self.s)
-        self.assertEqual(assert_almost_equal(linear_interpolation(p_values, self.thresholds),[interpolation(np.sort(p_values)[:i], self.thresholds) for i in range(1, self.s+1)] ), None)
-        self.assertEqual(linear_interpolation(p_values, self.thresholds, kmin=self.kmin)[-1], interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.K))
+        self.assertEqual(assert_almost_equal(linear_interpolation(p_values, self.thresholds),[interpolation(np.sort(p_values)[:i], self.thresholds, zeta=self.zeta) for i in range(1, self.s+1)] ), None)
+        self.assertEqual(linear_interpolation(p_values, self.thresholds, kmin=self.kmin)[-1], interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.K, zeta=self.zeta))
 
         p_values = np.random.beta(a=self.K, b=self.m - self.K + 1, size=self.s)
-        self.assertEqual(assert_almost_equal(linear_interpolation(p_values, self.thresholds),[interpolation(np.sort(p_values)[:i], self.thresholds) for i in range(1, self.s+1)] ), None)
-        self.assertEqual(linear_interpolation(p_values, self.thresholds, kmin=self.kmin)[-1], interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.K))
+        self.assertEqual(assert_almost_equal(linear_interpolation(p_values, self.thresholds),[interpolation(np.sort(p_values)[:i], self.thresholds, zeta=self.zeta) for i in range(1, self.s+1)] ), None)
+        self.assertEqual(linear_interpolation(p_values, self.thresholds, kmin=self.kmin)[-1], interpolation_minmax(p_values, self.thresholds, kmin=self.kmin, kmax=self.K, zeta=self.zeta))
 
 
 if __name__ == '__main__':
